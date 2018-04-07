f043b2c479c692a859a044d0127b867b93c156a4
Aditya Rao <hello@adityarao.name>
2018-04-07 21:24:32 +0530

    Added views and urls for home page

--- visa_app/models.py
+++ visa_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Country(models.Model):
     country_name = models.CharField(max_length=200)
+   
     country_visa_choices = (
         ('1', 'No visa required'),
         ('2', 'Visa on arrival'),
@@ -18,5 +19,9 @@ class Country(models.Model):
         choices=country_visa_choices,
         default=7,
     )
+
+
+    def __str__(self):
+        return self.country_name
 
 
